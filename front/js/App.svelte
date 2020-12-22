27e0c49c51a4198f1cabf8ae8969389e7c375cdf
debrsky <debrsky@gmail.com>
2020-12-22 15:50:22 +1000

    рабочий фронт

--- front/js/App.svelte
+++ front/js/App.svelte
@@ -1,118 +1,153 @@
 <script>
-	import {getAccount, putValue} from './utils';
-	import Meter from './Meter.svelte';
+	import {getAccount, getMeter, putValue} from './utils';
 	
 	const meterTypes = ['ЭЛ', 'ГВ', 'ХВ'];
 	const meterTypeNames = {
-		"ЭЛ": "Электричество",
-		"ГВ": "Горячая вода",
-		"ХВ": "Холодная вода"		
+		"ЭЛ": "⚡ Электричество",
+		"ГВ": "♨ Горячая вода",
+		"ХВ": "❄ Холодная вода"
 	}
 	let currentMeterType = meterTypes[0];
 	let accountToCheck = "";
 	let accountPromise;
-	let account;
+
+	let meterToCheck = "";
+	let meterPromise;
+	let meter;
+
+	let reading = "";
+
+	let transmitPromise;
 	
-	async function transmitHandler ({accountNo, number, value}) {
-		await putValue({accountNo, number, value});
-	};
-	
-	$: {
-		accountPromise = getAccount(accountToCheck); 
-		account = null;
-		(async () => {
-			account = await accountPromise;
-			// console.log(account);
-		})().catch(()=>{});
+	function transmitHandler () {
+		transmitPromise = putValue(accountToCheck, meterToCheck, reading);
 	};
 
+	$: {
+		accountPromise = getAccount(accountToCheck); 
+		// meterToCheck = "";
+	}
+
+	$: {
+		meterPromise = getMeter(accountToCheck, meterToCheck);
+		(async () => { meter = null; meter = await meterPromise; })().catch(()=>{});
+	}
+	
 </script>
 
 <style>
-	main {
-		max-width: 420px;
+	article {
+		background-color: beige;
+		padding: 5px;
 	}
 
-	.visuallyhidden {
-		position: absolute;
-
-		width: 1px;
-		height: 1px;
-		margin: -1px;
-		border: 0;
-		padding: 0;
-
-		white-space: nowrap;
-
-		clip-path: inset(100%);
-		clip: rect(0 0 0 0);
-		overflow: hidden;
-	}	
-	
-	.radio {
-		display: inline;
-		border-bottom: 1px solid teal;
-		cursor: pointer;
-		font-weight: bold;
-		color: inherit;
-	}
-	
-	.radio input:not(:checked) + span {
-		color: #999;
-	}
-	
-	.radio input:checked + span {
-		display: block;
-		border-bottom: 4px solid teal;
-		font-weight: bold;
-	}
-	
-	.tabs {
+	.control {
 		display: flex;
-		/* outline: 1px solid red; */
-		margin: 0.5em 0 0.5em 0;
+		flex-wrap: wrap;
+		position: relative;
+		margin: 15px 0;
+		width: 100%;
+		/* border: 1px solid red; */
+	}
+
+	.control__input {
+		margin-left: auto;
+		padding-right: 2.3ch;
+		width: 15ch;
+		font-family: 'Courier New', Courier, monospace;
+		font-weight: bold;
+		font-size: 20px;
+	}
+
+	.control__state-icon {
+		position: absolute;
+		right: 3px;
+		top: 5px;
+		font-family: 'Courier New', Courier, monospace;
+		font-weight: bold;
+		font-size: 20px;
+	}
+
+	.control__state-text {
+		width: 100%;
 	}
 	
-	.tab {
-		min-width: 4em;
-		text-align: center;
-		margin: 0 3px;
+	.control__text {
+		margin-left: auto;
+		font-family: Arial, Helvetica, sans-serif;
+		font-weight: bold;
+		font-size: 18px;
+		width: 180px;
+		border-bottom: 1px solid teal;
 	}
+
+	.control__send-button {
+		padding: 15px;
+		background-color: teal;
+		color: white;
+		font-weight: bold;
+		font-size: 20px;
+		cursor: pointer;
+	}
+
 </style>
 
-<main>
+<article>
 <hgroup>
-<h1>Передача показаний счетчиков</h1>
-<p>
+<h2 class="title">Передача показаний счетчиков</h2>
+<p class="subtitle">
 	Показания счетчиков передаются в период с 20 по 25 число каждого месяца.
 </p>
 </hgroup>
 
-<p style="text-align: center; font-style: italic;">
-	Демо счета: 5233, 1523, 3223
-</p>
-
-<label>Лицевой счет <input type=text bind:value={accountToCheck}>
-{#await accountPromise}
-	<span>...поиск</span>
-{:then account}
-	<span style="color: green">✔</span>
-{:catch error}
-	<span style="color: red">{accountToCheck ? "❌" : " "}</span>
-{/await}
+<label class="control">
+	<span class="control__title">Лицевой счет</span>
+	<input class="control__input" type=text bind:value={accountToCheck}>
+	<span class="control__state-icon">
+		{#await accountPromise}🔍
+		{:then account}✔
+		{:catch error}{accountToCheck ? "❌" : " "}
+		{/await}
+	</span>
 </label>
 
-{#if account}
-	<form>
-		<div class=tabs>
-			{#each meterTypes as meterType}
-			<label class="radio tab"><input class="visuallyhidden" type=radio bind:group={currentMeterType} value={meterType}> <span>{meterTypeNames[meterType]}</span> </label> 
-			{/each}
-		</div>
-	</form>
 
-	{#each account.meters.filter(meter => meter.meterType === currentMeterType) as meter (meter.number)}
-		<Meter meter={{accountNo: account.accountNo, ...meter}} {transmitHandler}></Meter>
-	{/each}
-{/if}
-</main>
+<label class="control">
+	<span class="control__title">Номер счетчика</span>
+	<input class="control__input" type=text bind:value={meterToCheck}>
+	<span class="control__state-icon">
+		{#await meterPromise}🔍
+		{:then meter}✔
+		{:catch error}{meterToCheck ? "❌" : " "}
+		{/await}
+	</span>
+</label>
+
+<div class="control">
+	<span class="control__title">Тип счетчика</span>
+	<span class="control__text">{meterTypeNames[meter?.type] || ""}</span>
+</div>
+
+<div class="control">
+	<span class="control__title">Описание</span>
+	<span class="control__text">{meter?.memo  || ""}</span>
+</div>
+
+<label class="control">
+	<span class="control_title">Показания</span>
+	<input class="control__input" type=text bind:value={reading}>
+</label>
+
+<div class="control">
+	<button class="control__send-button" on:click={transmitHandler}>Передать показания</button>
+	{#if transmitPromise}
+		<div class="control__state-text">
+			{#await transmitPromise} отправляю...
+			{:then ok} ✔ показания переданы 
+			{:catch} ❌ ошибка передачи показаний
+			{/await}
+		</div>
+	{/if}
+</div>
+
+</article>
--- front/js/utils.js
+++ front/js/utils.js
@@ -1,9 +1,10 @@
 import accounts from './data';
+const accountNumbers = Object.keys(accounts);
 
-async function getAccount (accountNo) {
-	const accountsFiltered = accounts.filter(
-		account => {
-			return parseInt(account.accountNo) === parseInt(accountNo)
+async function getAccount (accountNoToCheck) {
+	const accountsFiltered = accountNumbers.filter(
+		accountNumber => {
+			return parseInt(accountNumber) === parseInt(accountNoToCheck)
 		}
 	);
 	
@@ -14,12 +15,23 @@ async function getAccount (accountNo) {
 	return accountsFiltered[0];
 }
 
-async function putValue ({accountNo, number, value}) {
-	const account = await getAccount(accountNo);
-	const meter = account.meters.find(meter => parseInt(meter.number) === parseInt(number));
+async function getMeter (accountNo, meterNoToCheck) {
+	const account = accounts[accountNo];
+	const meter = account.meters[meterNoToCheck];
+
+	await new Promise( resolve => setTimeout(resolve, 500 + Math.floor(Math.random() * 500)) );
+	
+	if (!meter) throw Error("Счетчик не найден");
+	
+	return meter;
+}
+
+
+async function putValue (accountNo, meterNo, reading) {
+	const meter = await getMeter(accountNo, meterNo);
 	if (!meter) throw Error("Не удалось передать показания");
-	meter.currentValue = value;
+	meter.currentReading = reading;
 	return true;
 }
 
-export {getAccount, putValue};
+export {getAccount, getMeter, putValue};
